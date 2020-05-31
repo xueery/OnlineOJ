f813c35b011db76e5fc1bb4f491b7fbc72e3a09d
xueery <2042064451@qq.com>
2020-05-31 12:48:41 +0800

    Problem类

---
+++ src/main/java/problem/Problem.java
@@ -0,0 +1,75 @@
+package problem;
+
+/**
+ * @author:wangxue
+ * @date:2020/5/17 16:10
+ */
+// 实体类，每个Problem 对象就对应到 oj_table 中的一条记录
+public class Problem {
+    private int id;
+    private String title;
+    private String level;
+    private String description;
+    private String templateCode;
+    private String testCode;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getLevel() {
+        return level;
+    }
+
+    public void setLevel(String level) {
+        this.level = level;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getTemplateCode() {
+        return templateCode;
+    }
+
+    public void setTemplateCode(String templateCode) {
+        this.templateCode = templateCode;
+    }
+
+    public String getTestCode() {
+        return testCode;
+    }
+
+    public void setTestCode(String testCode) {
+        this.testCode = testCode;
+    }
+
+    @Override
+    public String toString() {
+        return "problem{" +
+                "id=" + id +
+                ", title='" + title + '\'' +
+                ", level='" + level + '\'' +
+                ", description='" + description + '\'' +
+                ", templateCode='" + templateCode + '\'' +
+                ", testCode='" + testCode + '\'' +
+                '}';
+    }
+}
